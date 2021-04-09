e6a96842e706b8804613e6955cb104f3adc1e140
fernando <fernando.k.lozano@gmail.com>
2021-04-09 16:39:42 -0500

    added selection sort

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Helps me to brush up on different sorting algorithms
 The intent is to make a few different sorting algorithms and displaying visually how they perform.
 
 Might add timing
+
+Will be displaying:
+    Bubble Sort.
+    Selection Sort.
+    Insertion Sort.
+    Merge Sort.
--- script.js
+++ script.js
@@ -1,12 +1,173 @@
-var bubbleDisplay = document.querySelectorAll(".display")[0];
+// colors used
+var colors = {
+    main: "#6b5b95",
+    sorted: "#13CE66",
+    compare: "#FF4949",
+    temp: "orange"
+}
 
 // *** bubble sort ***
 
 // most of this section was taken from 
 // https://www.geeksforgeeks.org/bubble-sort-visualization-using-javascript/
 
+// time complexity of O(n^2)
+
+const   bubbleDisplay = document.querySelectorAll(".display")[0],
+        bubbleSortBtn = document.querySelector("#bubbleSort");
+
+// Calling generatearray function
+generatearray(bubbleDisplay);
+
+bubbleSortBtn.addEventListener("click", async function () {
+    this.classList.toggle("disabled");
+    await BubbleSort();
+    this.classList.toggle("disabled");
+});
+
+// Promise to swap two blocks
+function bubbleSwap(el1, el2) {
+    return new Promise((resolve) => {
+
+        // For exchanging styles of two blocks
+        el1.classList.toggle("leftSwap");
+        el2.classList.toggle("rightSwap");
+        
+        window.requestAnimationFrame(function () {
+            
+            // For waiting for .25 sec
+            setTimeout(async () => {
+                el1.classList.toggle("leftSwap");
+                el2.classList.toggle("rightSwap");
+                bubbleDisplay.insertBefore(el2, el1);
+
+                resolve();
+            }, 250);
+        });
+    });
+}
+
+// Asynchronous BubbleSort function
+async function BubbleSort(delay = 100) {
+    let blocks = document.querySelectorAll("#one .block");
+
+    // BubbleSort Algorithm
+    for (let i = 0; i < blocks.length; i += 1) {
+        for (let j = 0; j < blocks.length - i - 1; j += 1) {
+
+            // To change background-color of the
+            // blocks to be compared
+            blocks[j].style.backgroundColor = colors.compare;
+            blocks[j + 1].style.backgroundColor = colors.compare;
+
+            // To wait for .1 sec
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, delay)
+            );
+
+            console.log("run");
+            let value1 = Number(blocks[j].dataset.value);
+            let value2 = Number(blocks[j + 1].dataset.value);
+
+            // To compare value of two blocks
+            if (value1 > value2) {
+                await bubbleSwap(blocks[j], blocks[j + 1]);
+                blocks = document.querySelectorAll("#one .block");
+            }
+
+            // Changing the color to the previous one
+            blocks[j].style.backgroundColor = colors.main;
+            blocks[j + 1].style.backgroundColor = colors.main;
+        }
+
+        //changing the color of greatest element 
+        //found in the above traversal
+        blocks[blocks.length - i - 1]
+            .style.backgroundColor = colors.sorted;
+    }
+}
+
+// *** selection sort ***
+
+const   selectionDisplay = document.querySelectorAll(".display")[1],
+        selectionSortBtn = document.querySelector("#selectionSort");
+
+// Calling generatearray function
+generatearray(selectionDisplay);
+
+selectionSortBtn.addEventListener("click", async function() {
+    this.classList.toggle("disabled");
+    await selectionSort();
+    this.classList.toggle("disabled");
+});
+
+async function selectionSort(delay = 100) {
+    let blocks = document.querySelectorAll("#two .block");
+
+    for (let i = 0; i < blocks.length-1; i++) {
+        // To change background-color of the
+        // blocks to be compared
+        blocks[i].style.backgroundColor = colors.compare;
+        
+        let value1 = Number(blocks[i].dataset.value);
+        let temp = i;
+        for (let j = i+1; j < blocks.length; j++) {
+            // To change background-color of the
+            // blocks to be compared
+            blocks[j].style.backgroundColor = colors.compare;
+            
+            // To wait for .1 sec
+            await new Promise((resolve) =>
+            setTimeout(() => {
+                resolve();
+            }, delay)
+            );
+
+            let value2 = Number(blocks[j].dataset.value);
+
+            if (value2 < value1) {
+                (temp!==i)?blocks[temp].style.backgroundColor = colors.main:"";
+                temp = j;
+                blocks[temp].style.backgroundColor = colors.temp;
+                value1 = Number(blocks[temp].dataset.value);
+            }
+            else {
+                blocks[j].style.backgroundColor = colors.main;
+            }
+
+        }
+        // swaps the two
+        if (temp !== i) {
+            selectionSwap(blocks[i], blocks[temp]);
+            blocks[i].style.backgroundColor = colors.sorted;
+            blocks[temp].style.backgroundColor = colors.main;
+        }
+        else {
+            blocks[i].style.backgroundColor = colors.sorted;
+        }
+    }
+    blocks[blocks.length-1].style.backgroundColor = colors.sorted;
+}
+
+function selectionSwap(el1, el2) {
+    // swap heights
+    let temp = el1.style.height;
+    el1.style.height = el2.style.height;
+    el2.style.height = temp;
+
+    // swap values
+    temp = el1.dataset.value;
+    el1.dataset.value = el2.dataset.value;
+    el2.dataset.value = temp;
+}
+
+
+// *** helpers ***
+
 // Function to generate the array of blocks
-function generatearray() {
+function generatearray(container) {
     for (var i = 0; i < 20; i++) {
 
         // Return a value from 1 to 250 (both inclusive)--height of container
@@ -20,75 +181,10 @@ function generatearray() {
 
         // Adding style to div
         array_ele.style.height = `${value}px`;
-        // array_ele.style.transform = `translate(${i * 6}px)`; might remove permanently if i can make alternative
-
         // data attribute to store value
         array_ele.dataset.value = value;
 
         // Appending created elements to index.html 
-        bubbleDisplay.appendChild(array_ele);
+        container.appendChild(array_ele);
     }
 }
-
-// Promise to swap two blocks
-function swap(el1, el2) {
-    return new Promise((resolve) => {
-
-        window.requestAnimationFrame(function () {
-
-            // For waiting for .25 sec
-            setTimeout(() => {
-                bubbleDisplay.insertBefore(el2, el1);           // make changes here!
-                resolve();
-            }, 10); //set to 250
-        });
-    });
-}
-
-// Asynchronous BubbleSort function
-async function BubbleSort(delay = 100) {
-    var blocks = document.querySelectorAll(".block");
-
-    // BubbleSort Algorithm
-    for (var i = 0; i < blocks.length; i += 1) {
-        for (var j = 0; j < blocks.length - i - 1; j += 1) {
-
-            // To change background-color of the
-            // blocks to be compared
-            blocks[j].style.backgroundColor = "#FF4949";
-            blocks[j + 1].style.backgroundColor = "#FF4949";
-
-            // To wait for .1 sec
-            await new Promise((resolve) =>
-                setTimeout(() => {
-                    resolve();
-                }, delay)
-            );
-
-            console.log("run");
-            var value1 = Number(blocks[j].dataset.value);
-            var value2 = Number(blocks[j + 1].dataset.value);
-
-            // To compare value of two blocks
-            if (value1 > value2) {
-                await swap(blocks[j], blocks[j + 1]);
-                blocks = document.querySelectorAll(".block");
-            }
-
-            // Changing the color to the previous one
-            blocks[j].style.backgroundColor = "#6b5b95";
-            blocks[j + 1].style.backgroundColor = "#6b5b95";
-        }
-
-        //changing the color of greatest element 
-        //found in the above traversal
-        blocks[blocks.length - i - 1]
-            .style.backgroundColor = "#13CE66";
-    }
-}
-
-// Calling generatearray function
-generatearray();
-
-// Calling BubbleSort function
-BubbleSort(10);
