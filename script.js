768af78b1edda44dcb794337b6c6dd50ca4ca3dd
Fernando <fernando.k.lozano@gmail.com>
2021-04-10 16:22:48 -0500

    started working on insertion sort

--- script.js
+++ script.js
@@ -25,28 +25,6 @@ bubbleSortBtn.addEventListener("click", async function () {
     this.classList.toggle("disabled");
 });
 
-// Promise to swap two blocks
-function bubbleSwap(el1, el2) {
-    return new Promise((resolve) => {
-
-        // For exchanging styles of two blocks
-        el1.classList.toggle("leftSwap");
-        el2.classList.toggle("rightSwap");
-        
-        window.requestAnimationFrame(function () {
-            
-            // For waiting for .25 sec
-            setTimeout(async () => {
-                el1.classList.toggle("leftSwap");
-                el2.classList.toggle("rightSwap");
-                bubbleDisplay.insertBefore(el2, el1);
-
-                resolve();
-            }, 250);
-        });
-    });
-}
-
 // Asynchronous BubbleSort function
 async function BubbleSort(delay = 100) {
     let blocks = document.querySelectorAll("#one .block");
@@ -88,6 +66,28 @@ async function BubbleSort(delay = 100) {
     }
 }
 
+// Promise to swap two blocks
+function bubbleSwap(el1, el2) {
+    return new Promise((resolve) => {
+
+        // For exchanging styles of two blocks
+        el1.classList.toggle("leftSwap");
+        el2.classList.toggle("rightSwap");
+
+        window.requestAnimationFrame(function () {
+
+            // For waiting for .25 sec
+            setTimeout(async () => {
+                el1.classList.toggle("leftSwap");
+                el2.classList.toggle("rightSwap");
+                bubbleDisplay.insertBefore(el2, el1);
+
+                resolve();
+            }, 250);
+        });
+    });
+}
+
 // *** selection sort ***
 
 // time complexity of O(n^2)
@@ -106,6 +106,102 @@ selectionSortBtn.addEventListener("click", async function() {
 
 async function selectionSort(delay = 100) {
     let blocks = document.querySelectorAll("#two .block");
+
+    for (let i = 0; i < blocks.length-1; i++) {
+        // To change background-color of the
+        // blocks to be compared
+        blocks[i].style.backgroundColor = colors.compare;
+        
+        let temp = i;
+        for (let j = i+1; j < blocks.length; j++) {
+            // To change background-color of the
+            // blocks to be compared
+            blocks[j].style.backgroundColor = colors.compare;
+            
+            // To wait for .1 sec
+            await new Promise((resolve) =>
+            setTimeout(() => {
+                resolve();
+            }, delay)
+            );
+            
+            let value1 = Number(blocks[temp].dataset.value);
+            let value2 = Number(blocks[j].dataset.value);
+            if (value1 > value2) {
+                (temp!==i)?blocks[temp].style.backgroundColor = colors.main:"";
+                temp = j;
+                blocks[temp].style.backgroundColor = colors.temp;
+            }
+            else {
+                blocks[j].style.backgroundColor = colors.main;
+            }
+
+        }
+        // swaps the two
+        if (temp !== i) {
+            await selectionSwap(blocks[i], blocks[temp], temp-i);
+            blocks = document.querySelectorAll("#two .block");
+            blocks[i].style.backgroundColor = colors.sorted;
+            blocks[temp].style.backgroundColor = colors.main;
+        }
+        else {
+            blocks[i].style.backgroundColor = colors.sorted;
+        }
+    }
+    blocks[blocks.length-1].style.backgroundColor = colors.sorted;
+}
+
+function selectionSwap(el1, el2, transform) {
+    return new Promise((resolve) => {
+        // add animations here
+        el1.classList.toggle("transition");
+        el1.style.transform = `translate(${transform * 18}px)`;
+        
+        el2.classList.toggle("transition");
+        el2.style.transform = `translate(-${transform * 18}px)`;
+
+        window.requestAnimationFrame(function () {
+
+            // For waiting for .25 sec
+            setTimeout(async () => {
+                // remove animations here
+                el1.classList.toggle("transition");
+                el1.style.transform = "";
+
+                el2.classList.toggle("transition");
+                el2.style.transform = "";
+                // swap heights
+                let temp = el1.style.height;
+                el1.style.height = el2.style.height;
+                el2.style.height = temp;
+            
+                // swap values
+                temp = el1.dataset.value;
+                el1.dataset.value = el2.dataset.value;
+                el2.dataset.value = temp;
+                resolve();
+            }, 250);
+        });
+    });
+}
+// *** Insertion sort ***
+
+// time complexity of O(n^2)
+
+const   insertionDisplay = document.querySelectorAll(".display")[2],
+        insertionSortBtn = document.querySelector("#insertionSort");
+
+// Calling generatearray function
+generatearray(insertionDisplay);
+
+insertionSortBtn.addEventListener("click", async function() {
+    this.classList.toggle("disabled");
+    await insertionSort();
+    this.classList.toggle("disabled");
+});
+
+async function insertionSort(delay = 100) {
+    let blocks = document.querySelectorAll("#three .block");                // i have changed code till here
 
     for (let i = 0; i < blocks.length-1; i++) {
         // To change background-color of the
